ba5d7c074b96bdd6b631b791b45f29e26470d103
taxguide <singhparash06@gmail.com>
2026-07-28 17:08:46 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'PC Mastercard Login'
+author = 'PC Mastercard Login'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "PC Mastercard Login"
